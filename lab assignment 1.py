6abb90ec360d846435c287baf6d7fd5e90259837
Yashikagup <128921191+Yashikagup@users.noreply.github.com>
2024-08-05 15:43:00 +0530

    Update lab assignment 1.py

--- lab assignment 1.py
+++ lab assignment 1.py
@@ -1,22 +1,22 @@
 #Q.1 print helloworld
 #Answer
-# print('HELLO WORLD')
+ print('HELLO WORLD')
 
 #OUTPUT:-
 # HELLO WORLD
 
 #Q.2 describe local variable and global variable code
 #Answer
- # x = 4
+  x = 4
 
-# def yashi_function():
-#     y = 6
-#     print("Inside the function, local variable y:", y)
-#     print("Inside the function, global variable x:", x)
+ def yashi_function():
+   y = 6
+    print("Inside the function, local variable y:", y)
+    print("Inside the function, global variable x:", x)
 
-# yashi_function()
+yashi_function()
 
-# print("Outside the function, global variable x:", x)
+ print("Outside the function, global variable x:", x)
 
 #OUTPUT:-
 
@@ -26,12 +26,12 @@
 
 #Q.3 Write a code that describe Indentation error
 #Answer
-# def gungun_function():
-#     x = 40
-#     y = 50
-#     if x < y:
-#     print("x is less than y")  # This line is incorrectly indented
-# gungun_function()
+def gungun_function():
+     x = 40
+    y = 50
+    if x < y:
+  print("x is less than y")  # This line is incorrectly indented
+ gungun_function()
 
 #OUTPUT:-
 
@@ -39,12 +39,12 @@
 
 #Q.4 write a code that describe local and global variable with same name
 #Answer
-# x = 60
-# def var_function():
-#     x = 10
-#     print("Inside the function, local variable x:", x)
-# var_function()
-# print("Outside the function, global variable x:", x)
+ x = 60
+def var_function():
+ x = 10
+    print("Inside the function, local variable x:", x)
+var_function()
+ print("Outside the function, global variable x:", x)
 
 #OUTPUT:-
 
@@ -53,14 +53,14 @@
 
 #Q.5 Write a code for string, int and float input
 #Answer 
- # my_string = " yashika gupta"
-# print("string = ",my_string)
+  my_string = " yashika gupta"
+ print("string = ",my_string)
 
-# my_int = "99"
-# print(" integer = ", my_int)
+ my_int = "99"
+ print(" integer = ", my_int)
 
-# my_float ="12"
-# print("float=", my_float)
+ my_float ="12"
+print("float=", my_float)
 
  #OUTPUT:-
 
